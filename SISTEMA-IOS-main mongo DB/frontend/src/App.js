96b02f5fbd6b331f2da6c9ab89a1354ad7e90603
jesielamarojunior-maker <jesiel.amarojunior@gmail.com>
2026-01-29 16:16:15 -0300

    Fix: Correção de caracteres UTF-8 no frontend

--- SISTEMA-IOS-main mongo DB/frontend/src/App.js
+++ SISTEMA-IOS-main mongo DB/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+﻿import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import axios from "axios";
